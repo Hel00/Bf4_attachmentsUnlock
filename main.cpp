31c187031a979ef2c75688196efe3ce6128821d5
Hel <11891920+Hel00@users.noreply.github.com>
2024-08-26 10:37:55 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+// zig build-exe -target x86_64-windows-none -L"/home/hel/.wine/drive_c/windows/system32/" -dynamic -lkernel32 -luser32 -cflags --std=c++20 -- --subsystem windows -fentry=Main -O ReleaseSmall -fstrip -flto --gc-sections -ffunction-sections main.cpp
+
+#include "types.hpp"
+#include "win32Utils.hpp"
+#include "memory.hpp"
+
+extern "C" void Main()
+{
+  static constexpr const char *processName = "Battlefield 4";
+  static constexpr auto PROCESS_ALL_ACCESS = (0x000F0000L | 0x00100000L | 0xFFF); // 2035711
+  static constexpr auto delay = 10'000;
+
+  DWORD processId;
+  HWND processHwnd = FindWindowA(0, processName);
+  GetWindowThreadProcessId(processHwnd, &processId);
+  HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+
+  uint64_t unlockEverything;
+
+  while (true)
+  {
+    unlockEverything = readMemory(processHandle, 0x1423717C0) + 0x54;
+    writeMemory(processHandle, unlockEverything, 1);
+
+    Sleep(delay);
+  }
+}
+
+extern "C" void WinMain() {}
+extern "C" void wWinMainCRTStartup() { Main(); }
+extern "C" void _fltused() {}
+extern "C" void _tls_index() {}
